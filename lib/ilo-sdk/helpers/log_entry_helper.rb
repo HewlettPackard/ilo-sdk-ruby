eb4eb23f42501440f894bd06d4a49c5c037373c6
Jared Smartt <jared.smartt@hpe.com>
2016-09-07 21:24:59 +0000

    Fixed log_entry helper

--- lib/ilo-sdk/helpers/log_entry_helper.rb
+++ lib/ilo-sdk/helpers/log_entry_helper.rb
@@ -1,7 +1,7 @@
-# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# (c) Copyright 2016 Hewlett Packard Enterprise Development LP
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
-# You may not use this file except in compliance with the License.
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software distributed
@@ -9,16 +9,31 @@
 # CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
+require 'time'
+
 module ILO_SDK
   # Contains helper methods for Log Entry actions
   module LogEntryHelper
+    # Get the URI for the specified log type
+    # @param [String, Symbol] log_type
+    # @raise [RuntimeError] if type is invalid
+    # @return [String] URI of log service
+    def uri_for_log_type(log_type, id = 1)
+      resource = case log_type.to_s.upcase
+                 when 'IEL' then 'Managers'
+                 when 'IML' then 'Systems'
+                 else raise "Invalid log_type '#{log_type}'. Valid options are IEL and IML"
+                 end
+      "/redfish/v1/#{resource}/#{id}/LogServices/#{log_type.upcase}/"
+    end
+
     # Clear the specified logs
     # @param [String, Symbol] log_type
     # @raise [RuntimeError] if the request failed
     # @return true
     def clear_logs(log_type)
       new_action = { 'Action' => 'ClearLog' }
-      response = rest_post("/redfish/v1/Managers/1/LogServices/#{log_type}/", body: new_action)
+      response = rest_post(uri_for_log_type(log_type), body: new_action)
       response_handler(response)
       true
     end
@@ -28,28 +43,25 @@ module ILO_SDK
     # @raise [RuntimeError] if the request failed
     # @return [TrueClass, FalseClass] logs_empty
     def logs_empty?(log_type)
-      response = rest_get("/redfish/v1/Managers/1/LogServices/#{log_type}/Entries/")
+      response = rest_get("#{uri_for_log_type(log_type)}Entries/")
       response_handler(response)['Items'].empty?
     end
 
     # Get the specified logs
-    # @param [String, Symbol] severity_level
-    # @param [String, Symbol] duration
-    # @param [String, Symbol] log_type
+    # @param [String, Symbol, NilClass] severity_level Set to nil to get all logs
+    # @param [String, Symbol] duration Up to this many hours ago
+    # @param [String, Symbol] log_type IEL or IML
     # @raise [RuntimeError] if the request failed
-    # @return logs
+    # @return [Array] log entries
     def get_logs(severity_level, duration, log_type)
-      response = rest_get("/redfish/v1/Managers/1/LogServices/#{log_type}/Entries/")
+      response = rest_get("#{uri_for_log_type(log_type)}Entries/")
       entries = response_handler(response)['Items']
-      logs = []
-      entries.each do |e|
-        if severity_level.nil?
-          logs.push("#{e['Severity']} | #{e['Message']} | #{e['Created']}") if Time.parse(e['Created']) > (Time.now.utc - (duration * 3600))
-        elsif e['Severity'] == severity_level && Time.parse(e['Created']) > (Time.now.utc - (duration * 3600))
-          logs.push("#{e['Severity']} | #{e['Message']} | #{e['Created']}")
-        end
+      start_time = Time.now.utc - (duration * 3600)
+      if severity_level.nil?
+        entries.select { |e| Time.parse(e['Created']) > start_time }
+      else
+        entries.select { |e| severity_level.to_s.casecmp(e['Severity']) == 0 && Time.parse(e['Created']) > start_time }
       end
-      logs
     end
   end
 end
